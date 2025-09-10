153566c2fc5e83ea385061677cbd292ec46af9fc
anacarolinaoleiro-glitch <anacarolina.oleiro@gmail.com>
2025-09-10 19:12:24 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('Servidor funcionando!'));
+
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
